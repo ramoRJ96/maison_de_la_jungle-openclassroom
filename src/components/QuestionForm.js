215bca1643234ceb9650a102d3c2e2d9a199c801
MOISE Rajesearison <moiseraidjy@gmail.com>
2021-06-01 11:17:06 +0300

    add all project

---
+++ src/components/QuestionForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function QuestionForm() {
+  const [inputValue, setInputValue] = useState("Posez votre question ici");
+
+  const isInputError = inputValue.includes("f");
+
+  function checkValue(value) {
+    if (!value.includes("f")) {
+      setInputValue(value);
+    }
+  }
+  return (
+    <div>
+      <textarea
+        value={inputValue}
+        onChange={(e) => checkValue(e.target.value)}
+      />
+      <button onClick={() => alert(inputValue)}>Alertez moi 🚨</button>
+
+      {isInputError && (
+        <div>🔥 Vous n'avez pas le droit d'utiliser la lettre "f" ici.</div>
+      )}
+    </div>
+  );
+}
+
+export default QuestionForm;
